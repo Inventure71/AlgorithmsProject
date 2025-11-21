a341c9bdc3d7ad87f8ae7ebe825fb69a88c0b02b
inventure71 <matteo.giorgetti.05@gmail.com>
2025-11-21 17:18:26 +0100

    latest working

--- game_loop.py
+++ game_loop.py
@@ -11,29 +11,40 @@ colors = {
     5: (200, 100, 100) # tower p2
 }
 
-rows = int(32*4) #32
+
+"""
+Variables for the game loop
+"""
+rows = int(32) #32
 cols = int(rows / 16 * 9)
 draw_borders = True
+screen = None
+clock = None
+arena = None
+tile_size = None
 
-# find optimal tile size
-tile_size = 800/rows # optimal for 32 is 25
+def setup_arena():
+    global screen, clock, arena, tile_size
+    # find optimal tile size
+    tile_size = 800/rows # optimal for 32 is 25
 
-pygame.init()
+    pygame.init()
 
-screen = pygame.display.set_mode((int(cols * tile_size), int(rows * tile_size)))
-clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((int(cols * tile_size), int(rows * tile_size)))
+    clock = pygame.time.Clock()
 
-arena = Arena(rows)
-arena.world_generation()
+    arena = Arena(rows)
+    arena.world_generation()
 
-location = (10, 2)
-simple_troop = Troop(name="simple_troop", health=100, damage=1, range=1, movement_speed=1, attack_type="melee", attack_speed=1, attack_range=1, attack_cooldown=1, size=1, color=(255, 0, 0), team=2, location=location, arena=arena)
-arena.spawn_unit(simple_troop, location)
+def add_test_troops():
+    global arena
+    location = (10, 2)
+    simple_troop = Troop(name="simple_troop", health=100, damage=1, range=1, movement_speed=1, attack_type="melee", attack_speed=1, attack_range=1, attack_cooldown=1, size=1, color=(255, 0, 0), team=2, location=location, arena=arena)
+    arena.spawn_unit(simple_troop, location)
 
-location = (rows-10, 2)
-simple_troop_team_2 = Troop(name="simple_troop", health=100, damage=1, range=1, movement_speed=2, attack_type="melee", attack_speed=1, attack_range=1, attack_cooldown=1, size=1, color=(255, 0, 0), team=1, location=location, arena=arena)
-arena.spawn_unit(simple_troop_team_2, location)
-
+    location = (rows-10, 2)
+    simple_troop_team_2 = Troop(name="simple_troop", health=100, damage=1, range=1, movement_speed=2, attack_type="melee", attack_speed=1, attack_range=1, attack_cooldown=1, size=1, color=(255, 0, 0), team=1, location=location, arena=arena)
+    arena.spawn_unit(simple_troop_team_2, location)
 
 def draw_arena():
     for y, row in enumerate(arena.grid):
@@ -61,6 +72,7 @@ def draw_units():
         pygame.draw.rect(screen, troop.color, (troop.location[1] * tile_size , troop.location[0] * tile_size, tile_size, tile_size))
 
 
+setup_arena()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
